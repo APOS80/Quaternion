96591d338f423f741f712150d0e20da93500f1e8
Andreas Olsson <photoguy.apo@gmail.com>
2017-10-13 07:36:45 +0200

    fixes

--- info.rkt
+++ info.rkt
@@ -1,11 +1,15 @@
 #lang info
 (define collection "quaternion")
 (define deps '("base"
-               "rackunit-lib"))
-(define build-deps '("scribble-lib"
+               "scribble-lib"
+               "typed-racket-lib"
+               "typed-racket-more"
+               "math-lib"))
+(define build-deps '("typed-racket-doc"
                      "racket-doc"
                      "math-doc"))
 (define scribblings '(["quaternion.scrbl" () ("3D")]))
 (define pkg-desc "Quaternions")
 (define version "1.0")
 (define pkg-authors '(APOS80))
+
